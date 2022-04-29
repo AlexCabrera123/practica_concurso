3311de35fdd040dc32755c361fec9e07cf76d4a9
AlexCabrera123 <l21121600@tecnm.morelia.mx>
2022-04-29 00:09:58 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int [] numeros = new int[8];
+        String etapa1 = "Inicial";
+        String etapa2 = "Colocar paracaidas";
+        String etapa3 = "Subida al avion";
+        String etapa4 = "Despegue";
+        String etapa5 = "Salto";
+        String etapa6 = "Apertura de paracaidas";
+        String etapa7 = "Aterrizaje";
+        String etapa8 = "Recuperacion";
+        int cont = 0;
+        int numeroMenor;
+
+        System.out.println("Las frecuencias son;");
+        for (int i = 0; i <= numeros.length; i++) {
+            numeros[i] = Integer.parseInt(br.readLine());
+        }
+
+        for (int i = 0; i <= numeros.length; i++) {
+            if ([i])
+        }
+
+    }
+}
